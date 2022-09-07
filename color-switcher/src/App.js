33bf9da61679701614729d5bad4e5087d87b1c9e
Alexis Mainwaring <leximainw@gmail.com>
2022-09-07 18:06:50 -0400

    separate buttons into ColorChangeButton component

--- color-switcher/src/App.js
+++ color-switcher/src/App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import './App.css';
+import ColorChangeButton from './ColorChangeButton';
 
 function App() {
   const [color, setColor] = useState('');
@@ -7,9 +8,14 @@ function App() {
     <div className={`react-root ${color}`}>
       <div className='centered'>
         <h1>Color Picker</h1>
-        <button className='red' onClick={() => setColor('red')}>red</button>
-        <button className='yellow' onClick={() => setColor('yellow')}>yellow</button>
-        <button className='blue' onClick={() => setColor('blue')}>blue</button>
+        <ColorChangeButton color='black' setColor={setColor} />
+        <ColorChangeButton color='red' setColor={setColor} />
+        <ColorChangeButton color='green' setColor={setColor} />
+        <ColorChangeButton color='yellow' setColor={setColor} />
+        <ColorChangeButton color='blue' setColor={setColor} />
+        <ColorChangeButton color='magenta' setColor={setColor} />
+        <ColorChangeButton color='cyan' setColor={setColor} />
+        <ColorChangeButton color='white' setColor={setColor} />
       </div>
     </div>
   );
