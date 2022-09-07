e14d8475a804e773acfdbaa65b5431819f4c1635
Alexis Mainwaring <leximainw@gmail.com>
2022-09-07 18:12:12 -0400

    wrap buttons with flexbox

--- color-switcher/src/App.js
+++ color-switcher/src/App.js
@@ -8,14 +8,16 @@ function App() {
     <div className={`react-root ${color}`}>
       <div className='centered'>
         <h1>Color Picker</h1>
-        <ColorChangeButton color='black' setColor={setColor} />
-        <ColorChangeButton color='red' setColor={setColor} />
-        <ColorChangeButton color='green' setColor={setColor} />
-        <ColorChangeButton color='yellow' setColor={setColor} />
-        <ColorChangeButton color='blue' setColor={setColor} />
-        <ColorChangeButton color='magenta' setColor={setColor} />
-        <ColorChangeButton color='cyan' setColor={setColor} />
-        <ColorChangeButton color='white' setColor={setColor} />
+        <div className='button-flex'>
+          <ColorChangeButton color='black' setColor={setColor} />
+          <ColorChangeButton color='red' setColor={setColor} />
+          <ColorChangeButton color='green' setColor={setColor} />
+          <ColorChangeButton color='yellow' setColor={setColor} />
+          <ColorChangeButton color='blue' setColor={setColor} />
+          <ColorChangeButton color='magenta' setColor={setColor} />
+          <ColorChangeButton color='cyan' setColor={setColor} />
+          <ColorChangeButton color='white' setColor={setColor} />
+        </div>
       </div>
     </div>
   );
