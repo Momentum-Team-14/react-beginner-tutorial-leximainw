8d88ab7e0805dffb6edfdc1c956c211068ba6015
Alexis Mainwaring <leximainw@gmail.com>
2022-09-07 18:27:06 -0400

    reorganize buttons

--- color-switcher/src/App.js
+++ color-switcher/src/App.js
@@ -10,13 +10,17 @@ function App() {
         <h1>Color Picker</h1>
         <div className='button-flex'>
           <ColorChangeButton color='black' setColor={setColor} />
-          <ColorChangeButton color='red' setColor={setColor} />
-          <ColorChangeButton color='green' setColor={setColor} />
-          <ColorChangeButton color='yellow' setColor={setColor} />
-          <ColorChangeButton color='blue' setColor={setColor} />
-          <ColorChangeButton color='magenta' setColor={setColor} />
-          <ColorChangeButton color='cyan' setColor={setColor} />
+          <ColorChangeButton color='gray' setColor={setColor} />
           <ColorChangeButton color='white' setColor={setColor} />
+          <ColorChangeButton color='yellow' setColor={setColor} />
+          <ColorChangeButton color='brown' setColor={setColor} />
+          <ColorChangeButton color='green' setColor={setColor} />
+          <ColorChangeButton color='red' setColor={setColor} />
+          <ColorChangeButton color='lime' setColor={setColor} />
+          <ColorChangeButton color='blue' setColor={setColor} />
+          <ColorChangeButton color='purple' setColor={setColor} />
+          <ColorChangeButton color='cyan' setColor={setColor} />
+          <ColorChangeButton color='magenta' setColor={setColor} />
         </div>
       </div>
     </div>
