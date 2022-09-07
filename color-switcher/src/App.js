0e43c6e78fb9b2303f4fdbbeb735fa993ca78583
Alexis Mainwaring <leximainw@gmail.com>
2022-09-07 17:21:37 -0400

    change color on click

--- color-switcher/src/App.js
+++ color-switcher/src/App.js
@@ -1,13 +1,15 @@
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('');
   return (
-    <div className='react-root'>
+    <div className={`react-root ${color}`}>
       <div className='centered'>
         <h1>Color Picker</h1>
-        <button className='red'>red</button>
-        <button className='blue'>blue</button>
-        <button className='yellow'>yellow</button>
+        <button className='red' onClick={() => setColor('red')}>red</button>
+        <button className='yellow' onClick={() => setColor('yellow')}>yellow</button>
+        <button className='blue' onClick={() => setColor('blue')}>blue</button>
       </div>
     </div>
   );
